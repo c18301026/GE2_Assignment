1d30cafd5cf3064aa89e56c43a76f1eda14aa154
unknown <c18301026@mytudublin.ie>
2022-05-11 22:14:53 +0100

    Added scene 1 director script

---
+++ Borg Battle/Assets/Scripts/Scene1Director.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1Director : MonoBehaviour
+{
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+}
