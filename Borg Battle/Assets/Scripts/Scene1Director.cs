a0d2e8d41a65de53b0889429aa31484a8536bee4
unknown <c18301026@mytudublin.ie>
2022-05-12 01:59:50 +0100

    Added more camera angles to scene 1

--- Borg Battle/Assets/Scripts/Scene1Director.cs
+++ Borg Battle/Assets/Scripts/Scene1Director.cs
@@ -8,6 +8,7 @@ public class Scene1Director : MonoBehaviour
 	private GameObject target;
 	private Camera camera;
 	private float targetSpeed = 5f;
+	private bool lookAt;
 
 	void Awake()
 	{
@@ -26,6 +27,45 @@ public class Scene1Director : MonoBehaviour
 
 	void Update()
 	{
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			CameraAngle1();
+		}
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			CameraAngle2();
+		}
+		if(Input.GetKeyDown(KeyCode.T))
+		{
+			CameraAngle3();
+		}
+	}
+
+	void FixedUpdate()
+	{
 		target.transform.position += new Vector3(0, 0, targetSpeed * Time.deltaTime);
+
+		if(lookAt)
+		{
+			camera.transform.LookAt(enterprise.transform);
+		}
+	}
+
+	void CameraAngle1()
+	{
+		lookAt = true;
+		camera.transform.position = new Vector3(enterprise.transform.position.x - 10, enterprise.transform.position.y + 5, enterprise.transform.position.z + 25);
+	}
+
+	void CameraAngle2()
+	{
+		lookAt = true;
+		camera.transform.position = new Vector3(enterprise.transform.position.x + 10, enterprise.transform.position.y + 5, enterprise.transform.position.z + 25);
+	}
+
+	void CameraAngle3()
+	{
+		lookAt = true;
+		camera.transform.position = new Vector3(enterprise.transform.position.x, enterprise.transform.position.y + 10, enterprise.transform.position.z + 50);
 	}
 }
