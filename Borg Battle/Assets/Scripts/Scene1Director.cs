5906af362792e7516807190f8c5958cd4393630a
unknown <c18301026@mytudublin.ie>
2022-05-13 19:51:47 +0100

    Scene 1 finished!

--- Borg Battle/Assets/Scripts/Scene1Director.cs
+++ Borg Battle/Assets/Scripts/Scene1Director.cs
@@ -23,7 +23,13 @@ public class Scene1Director : MonoBehaviour
 						"DATA:\nAye sir.",
 						"FLEET COMMUNICATIONS:\nFlagship to Endeavor. Standby to engage at grid A-fifteen. ...Defiant and Bozeman, fall back to mobile position one. ...Acknowledge. ...We have it in visual range. A Borg cube on course zero point two one five, speed warp point nine six.",
 						"BORG COMMUNICATIONS:\nWe are the Borg. Lower your shields and surrender your ships. We will add your biological and technological distinctiveness to our own. Your culture will adapt to service us. Resistance is futile.",
-						"FLEET COMMUNICATIONS:\nAll units open fire. ...They've broken through the defence perimeter. ... Continue to attack. ...We need reinforcements. ...Ninety-six dead and twenty-two wounded on the Lexington."
+						"FLEET COMMUNICATIONS:\nAll units open fire. ...They've broken through the defence perimeter. ... Continue to attack. ...We need reinforcements. ...Ninety-six dead and twenty-two wounded on the Lexington.",
+						"PICARD:\nLieutenant Hawk. Set a course for Earth.",
+						"HAWK:\nAye sir.",
+						"PICARD:\nMaximum warp. ...I am about to commit a direct violation of our orders. Any of you who wish to object should do so now. It will be noted in my log.",
+						"DATA:\nCaptain, I believe I speak for everyone here, sir, when I say ...to hell with our orders.",
+						"PICARD:\nRED ALERT! All hands to battle stations.",
+						"PICARD:\nEngage."
 						};
 	private float[] displayTimeStamps = new float[] {	0f,
 								2.2f,
@@ -32,13 +38,23 @@ public class Scene1Director : MonoBehaviour
 								15f,
 								18.25f,
 								32.4f,
-								48.8f
+								48.8f,
+								72f,
+								75f,
+								76.5f,
+								92f,
+								105f,
+								109.5f
 								};
 	private float[] hideTimeStamps = new float[] {	9f,
-							17f};
+							17f,
+							90f,
+							102.5f,
+							109f,
+							112.5f};
 	private GameObject canvas, dialogueBox, portraitBox;
 	private Dialogue dialogueScript;
-	private Texture2D crusher, data, hawk, picard, riker, troi, connOfficer, wolf;
+	private Texture2D borg, crusher, data, hawk, picard, riker, starfleet, troi, connOfficer, worf;
 	private Texture2D[] portraits;
 
 	void Awake()
@@ -56,23 +72,31 @@ public class Scene1Director : MonoBehaviour
 		dialogueBox = GameObject.FindWithTag("DialogueBox");
 		portraitBox = GameObject.FindWithTag("PortraitBox");
 
-		crusher = Resources.Load("Portraits/crusher") as Texture2D;
-		data = Resources.Load("Portraits/data") as Texture2D;
-		hawk = Resources.Load("Portraits/hawk") as Texture2D;
-		picard = Resources.Load("Portraits/picard") as Texture2D;
-		riker = Resources.Load("Portraits/riker") as Texture2D;
-		troi = Resources.Load("Portraits/troi") as Texture2D;
-		connOfficer = Resources.Load("Portraits/connOfficer") as Texture2D;
-		wolf = Resources.Load("Portraits/wolf") as Texture2D;
+		borg = Resources.Load("Portraits/Borg") as Texture2D;
+		crusher = Resources.Load("Portraits/Crusher") as Texture2D;
+		data = Resources.Load("Portraits/Data") as Texture2D;
+		hawk = Resources.Load("Portraits/Hawk") as Texture2D;
+		picard = Resources.Load("Portraits/Picard") as Texture2D;
+		riker = Resources.Load("Portraits/Riker") as Texture2D;
+		starfleet = Resources.Load("Portraits/Starfleet") as Texture2D;
+		troi = Resources.Load("Portraits/Troi") as Texture2D;
+		connOfficer = Resources.Load("Portraits/USS_Defiant_Conn_Officer") as Texture2D;
+		worf = Resources.Load("Portraits/Worf") as Texture2D;
 
 		portraits = new Texture2D[] {	troi,
 						picard,
 						troi,
 						picard,
 						data,
+						starfleet,
+						borg,
+						starfleet,
+						picard,
+						hawk,
+						picard,
 						data,
-						data,
-						data
+						picard,
+						picard
 						};
 
 		dialogueScript = dialogueBox.GetComponent<Dialogue>();
@@ -86,27 +110,12 @@ public class Scene1Director : MonoBehaviour
 
 	void Start()
 	{
-	}
-
-	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.E))
-		{
-			CameraAngle(-10f, 5f, 25f);
-		}
-		if(Input.GetKeyDown(KeyCode.R))
-		{
-			CameraAngle(10f, 5f, 25f);
-		}
-		if(Input.GetKeyDown(KeyCode.T))
-		{
-			CameraAngle(0f, 10f, 50f);
-		}
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			turningAround = true;
-			Invoke("GoBack", 0.5f);
-		}
+		StartCoroutine(ChangeCameraAngle(10f, 5f, 25f, 11.5f));
+		StartCoroutine(ChangeCameraAngle(0f, 10f, 50f, 32.4f));
+		StartCoroutine(ChangeCameraAngle(-10f, 5f, 25f, 72f));
+		StartCoroutine(ChangeCameraAngle(0f, 10f, 50f, 92f));
+		StartCoroutine(ChangeCameraAngle(10f, 5f, 25f, 109.5f));
+		Invoke("TurnAround", 111.5f);
 	}
 
 	void FixedUpdate()
@@ -125,16 +134,24 @@ public class Scene1Director : MonoBehaviour
 			enterprise.transform.position += new Vector3(0, 0, targetSpeed * Time.deltaTime);
 			target.transform.position += new Vector3(0, 0, targetSpeed * Time.deltaTime);
 		}
+
 		if(lookAt)
 		{
 			camera.transform.LookAt(enterprise.transform);
 		}
 	}
 
-	void CameraAngle(float x, float y, float z)
+	IEnumerator ChangeCameraAngle(float x, float y, float z, float timeStamp)
 	{
+		yield return new WaitForSeconds(timeStamp);
 		lookAt = true;
 		camera.transform.position = new Vector3(enterprise.transform.position.x + x, enterprise.transform.position.y + y, enterprise.transform.position.z + z);
+	}
+
+	void TurnAround()
+	{
+		turningAround = true;
+		Invoke("GoBack", 0.5f);
 	}
 
 	void GoBack()
