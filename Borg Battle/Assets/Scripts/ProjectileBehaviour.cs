98663d00e9aa50e4d2ef0a35854f60dca3d3e944
unknown <c18301026@mytudublin.ie>
2022-05-14 20:02:04 +0100

    Explosion!

--- Borg Battle/Assets/Scripts/ProjectileBehaviour.cs
+++ Borg Battle/Assets/Scripts/ProjectileBehaviour.cs
@@ -8,13 +8,15 @@ public class ProjectileBehaviour : MonoBehaviour
 
 	private Vector3 toTarget, desired;
 	private float maxSpeed = 75f;
+	private GameObject explosion;
 
 	void Start()
 	{
+		explosion = Resources.Load("Prefabs/Explosion") as GameObject;
 		toTarget = target.transform.position - transform.position;
 		desired = toTarget.normalized * maxSpeed;
 		transform.LookAt(target.transform.position);
-		Invoke("DestroySelf", 1f);
+		Invoke("DestroySelf", 3f);
 	}
 
 	void Update()
@@ -22,15 +24,14 @@ public class ProjectileBehaviour : MonoBehaviour
 		transform.position += desired * Time.deltaTime;
 	}
 
-	/*
 	private void OnCollisionEnter(Collision collision)
 	{
 		if(collision.gameObject == target)
 		{
-			
+			Instantiate(explosion, transform.position, transform.rotation);
+			DestroySelf();	
 		}
 	}
-	*/
 
 	void DestroySelf()
 	{
